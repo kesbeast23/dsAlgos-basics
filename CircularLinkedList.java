b0be230f3c302142a4dc4591d214159c097f7d12
Kes <kesegomokgosi23@gmail.com>
2020-08-02 13:03:54 +0200

    ses1

--- CircularLinkedList.java
+++ CircularLinkedList.java
@@ -1,4 +1,4 @@
-import java.util.*;
+
 
 public class CircularLinkedList {
     public int size=0;
--- arrays/Main.java
+++ arrays/Main.java
@@ -1,6 +1,11 @@
-public class Main{
+public class Main extends Array{
     
     public static void main(String[] args) {
-        
+        Array numbers = new Array(4);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(40);
+        numbers.insert(2);
+        System.out.println( numbers.indexOf(40));
     }
 }
